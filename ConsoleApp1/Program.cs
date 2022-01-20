263a3eb0c8d83ed17aa86996bbc8d4f5edd6047a
Kenan Kurda <intecbrussel2122@gmail.com>
2022-01-20 10:44:58 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+
+namespace DirectoryDemo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //string currentDir = Directory.GetCurrentDirectory();
+            //Console.WriteLine(currentDir);
+            //string directoryOne = @"\textfiles";
+            ////string directoryOne = ConfigurationManager.AppSettings["directoryname"];
+            //Directory.CreateDirectory(directoryOne);
+            //Directory.CreateDirectory(@"textfiles_1");
+
+            //if (Directory.Exists(@"DirOne"))
+            //{
+            //    Console.WriteLine("Directory allready exists!!!");
+            //}
+            //else
+            //{
+            //    Directory.CreateDirectory(@"DirOne");
+            //}
+           
+            Console.ReadKey();
+        }
+    }
+}
